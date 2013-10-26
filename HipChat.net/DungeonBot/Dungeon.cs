478d8086994173f66e1cf25a788263181decde98
Mason <mavhuntrx@gmail.com>
2013-10-26 10:58:19 -0700

    Added Dungeon.cs

---
+++ HipChat.net/DungeonBot/Dungeon.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonBot
+{
+    /// <summary>
+    /// Defines a dungeon 
+    /// </summary>
+    class Dungeon
+    {
+        /// <summary>
+        /// an array that holds the dungeon layout
+        /// </summary>
+        public Room[,] dungeonLayout = new Room[5, 5];
+
+        /// <summary>
+        /// dungeon constructor
+        /// </summary>
+        public Dungeon()
+        {
+            // todo
+        }
+
+        /// <summary>
+        /// method to generate a new dungeon
+        /// </summary>
+        public void GenerateDungeon()
+        {
+            // todo
+        }
+    }
+
+    /// <summary>
+    /// defines a room in the dungeon
+    /// </summary>
+    public class Room
+    {
+        /// <summary>
+        /// text to describe room when hero enters
+        /// </summary>
+        public string _roomDescription;
+
+
+    }
+}
